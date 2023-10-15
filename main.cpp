231b8b41a46f808e75e07e5955df28e27474b11b
piervalli <piervalli@gmail.com>
2023-10-16 00:05:07 +0200

    Added Mutex on write

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 #include <QSqlError>
 #include <QDebug>
 #include <QtConcurrent>
+#include <QMutex>
+QMutex globalMutex;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
@@ -33,10 +35,11 @@ int main(int argc, char *argv[])
     static auto func = [](int rows,int offset){
         QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE",QString::number(offset));
         db.setDatabaseName("mydatabase.db"); // Replace with your database name
-        db.setConnectOptions(QLatin1String("QSQLITE_ENABLE_REGEXP=1;QSQLITE_THREADSAFE=2;QSQLITE_BUSY_TIMEOUT=10000"));
+        db.setConnectOptions(QLatin1String("QSQLITE_ENABLE_REGEXP=1;QSQLITE_THREADSAFE=2;QSQLITE_BUSY_TIMEOUT=0"));
         if (!db.open()) {
             qDebug() << "Database connection failed";
         }
+        globalMutex.lock();
         db.transaction();
         while(rows>0){
             --rows;
@@ -47,18 +50,20 @@ int main(int argc, char *argv[])
             }
             query.bindValue(":id",offset);
             query.bindValue(":description",offset);
+
             if(!query.exec()){
                 qCritical() << query.lastError().databaseText();
             }
+
             if(!query.exec("update product set description ='1' where id =1;")){
                 qCritical() << query.lastError();
             }
         }
         db.commit();
-
+        globalMutex.unlock();
     };
     QtConcurrent::run(func,300000,0);
-    QtConcurrent::run(func,300000,300001);
+    QtConcurrent::run(func,300000,600001);
     db.commit();
     db.close();
     return 0;//a.exec();
